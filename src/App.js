fc96013402d1a1bf615ed6ff975f69dff0748d66
Mateusz <mateuszpiorowski@gmail.com>
2020-03-01 18:13:57 +0100

    init react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {ExchangeList} from "./components/exchange/ExchangeList";
+
+function App() {
+  return (
+    <div className="App">
+      <ExchangeList/>
+    </div>
+  );
+}
+
+export default App;
