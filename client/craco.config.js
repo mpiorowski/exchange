9273746e26927a96f7e5b4dfd8a7d19d448e6492
Mateusz <mateuszpiorowski@gmail.com>
2020-03-03 23:43:34 +0100

    clear config

--- client/craco.config.js
+++ client/craco.config.js
@@ -2,34 +2,9 @@ const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const WebpackBar = require("webpackbar");
 const CracoAntDesignPlugin = require("craco-antd");
 const path = require("path");
-const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 // Don't open the browser during development
 process.env.BROWSER = "none";
-
-const cspConfigPolicy = {
-    // 'default-src': "'self'",
-    // 'base-uri': "'self'",
-    // 'object-src': "'self'",
-    // 'form-action': "'self'",
-    // 'img-src': ["'self'", "data:"],
-    // 'script-src': ["'self'", "'unsafe-eval'"],
-    // 'style-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"]
-};
-
-const cspConfigHash = {
-    // enabled: true,
-    // hashingMethod: 'sha256',
-    // hashEnabled: {
-    //     'script-src': true,
-    //     'style-src': true
-    // },
-    // nonceEnabled: {
-    //     'script-src': true,
-    //     'style-src': false
-    // }
-};
-
 
 module.exports = {
     webpack: {
@@ -39,7 +14,6 @@ module.exports = {
             ...(process.env.NODE_ENV === "development"
                 ? [new BundleAnalyzerPlugin({ openAnalyzer: false })]
                 : []
-                // : [new cspHtmlWebpackPlugin(cspConfigPolicy, cspConfigHash)]
             )
         ]
     },
